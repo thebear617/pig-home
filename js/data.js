5e114174cd21704a17d7bf91b344ef6d091f3d25
莫开车 <1357953389@qq.com>
2026-07-18 01:55:52 +0800

    relationship: 关系时间线新增益田假日约会
    - 07-17 和过马路地铁去益田假日
    - 吃潇湘阁、逛宠物超市、喝柠季奶茶

--- js/data.js
+++ js/data.js
@@ -371,6 +371,15 @@ const xianTrips = [
     travelers: ['过马路', '耙耙柑'],
     transport: '地铁',
     note: '和宝宝一起买帽子逛街吃东西'
+  },
+  {
+    dest: '益田假日',
+    startDate: '2026-07-17',
+    endDate: '2026-07-17',
+    status: 'completed',
+    travelers: ['过马路', '耙耙柑'],
+    transport: '地铁',
+    note: '吃潇湘阁、逛宠物超市、喝柠季奶茶'
   }
 ];
 
