4dab928f89eb613269a1110df797c036c980acdf
莫开车 <1357953389@qq.com>
2026-08-02 16:03:07 +0800

    chore: 固定本地开发端口

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   base: process.env.SITE_BASE || '/',
+  vite: { server: { strictPort: true } },
   build: {
     format: 'directory',
   },
